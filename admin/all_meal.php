fec1f554a813d3459f0a4cb0a3ba9403c75ddd0a
Iriniya <molodcovairina@yandex.ru>
2017-06-09 00:44:34 +0400

    08-06-2017

--- admin/all_meal.php
+++ admin/all_meal.php
@@ -1,36 +1,44 @@
 <?include("../db.php");
-if (isset($_GET['id']));{$id=($_GET['id']);}
-if (isset($_POST['id']))  {$id=$_POST['id']; }
-if (isset($_POST['food']))  {$food=$_POST['food']; }
-if (isset($_POST['weight']))  {$weight=$_POST['weight']; }
-if (isset($_POST['price']))  {$price=$_POST['price'];  }
+	if (isset($_GET['id']));{$id=($_GET['id']);}
+	if (isset($_POST['id']))  {$id=$_POST['id']; }
+	if (isset($_POST['food']))  {$food=$_POST['food']; }
+	if (isset($_POST['weight']))  {$weight=$_POST['weight']; }
+	if (isset($_POST['price']))  {$price=$_POST['price'];  }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Меню</title>
+	<link rel="stylesheet" type="text/css" href="../	style.css">
 </head>
 <body>
-
-<? if (isset($id)):?><h1>
-<?="Внесите изменения в блюдо" ;?></h1>
-<? $myrow=mysqli_query($db, "SELECT * FROM meal WHERE id='$id'");
-$rezult=mysqli_fetch_array($myrow);
-$time_id=$rezult['time_id'];
-?>
-<form name="edit_meal" method="post" action="edit_meal.php">
-	<label for="food"><p>Название блюда: </p></label>
-	<input id="food" type="text" name="food" value=<?=$rezult['food']?>><br>
-	<label for="weight"><p>Масса блюда: </p></label>
-	<input id="weight" type="text" name="weight" value=<?=$rezult['weight']?>><br>
-	<label for="price"><p>Цена блюда:</p></label>
-	<input id="price" type="text" name="price"  value=<?=$rezult['price']?>><br>
-	<input id="id" type="hidden" name="id" value=<?=$id?>><br>
-	<input type="submit" name="submit" value="Сохранить">
-</form>
+	<? if ($_GET['id']):?>
+	<h1>Внесите изменения в блюдо</h1>
+	<? $myrow=mysqli_query($db, "SELECT * FROM meal WHERE id='$id'");
+	$rezult=mysqli_fetch_array($myrow);
+	$time_id=$rezult['time_id'];
+	?>
+	<form name="edit_meal" method="post" action="all_meal.php">
+		<label for="food"><p>Название блюда: </p></label>
+		<input id="food" type="text" name="food" value=<?=$rezult["food"]?>><br>
+		<label for="weight"><p>Масса блюда: </p></label>
+		<input id="weight" type="text" name="weight" value=<?=$rezult['weight']?>><br>
+		<label for="price"><p>Цена блюда:</p></label>
+		<input id="price" type="text" name="price"  value=<?=$rezult['price']?>><br>
+		<input id="id" type="hidden" name="id" value=<?=$id?>><br>
+		<input type="submit" name="save_food" value="Сохранить">
+	</form>
+<?elseif ($_POST['id']):?>
+	<?$myrow_food=mysqli_query($db, "UPDATE meal SET food='$food',  weight='$weight', price='$price' WHERE id=$id");
+	if ($myrow_food==true) {
+		echo "<p> Блюдо $food обновлено</p>";
+	} else {
+		echo "<p> Блюдо НЕ обновлено</p>";
+	}
+	?>
 <?else:?>
-<? 
+	<? 
 	$myrow_all_meal=mysqli_query($db,"SELECT * FROM meal");
 	$rezult_all_meal=mysqli_fetch_array($myrow_all_meal);
 	do {
@@ -38,8 +46,7 @@ $time_id=$rezult['time_id'];
 	}
 	while ($rezult_all_meal=mysqli_fetch_array($myrow_all_meal)) ;
 	?>
-	<?endif?>
-
+<?endif?>
 
 <h3> <a href="../index.php"> На главную</a></h3>
 </body>
