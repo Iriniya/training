5e70cefb04ce4054768261d35b4553da6ef30cae
Iriniya <molodcovairina@yandex.ru>
2017-06-11 16:53:01 +0400

    11-06-2017

--- admin/all_meal.php
+++ admin/all_meal.php
@@ -21,12 +21,22 @@
 	?>
 	<form name="edit_meal" method="post" action="all_meal.php">
 		<label for="food"><p>Название блюда: </p></label>
-		<input id="food" type="text" name="food" value=<?=$rezult["food"]?>><br>
+		<input id="food" type="text" name="food" value="<?=$rezult["food"]?>"><br>
 		<label for="weight"><p>Масса блюда: </p></label>
 		<input id="weight" type="text" name="weight" value=<?=$rezult['weight']?>><br>
 		<label for="price"><p>Цена блюда:</p></label>
 		<input id="price" type="text" name="price"  value=<?=$rezult['price']?>><br>
 		<input id="id" type="hidden" name="id" value=<?=$id?>><br>
+		<select name="time_id">
+		<?
+		$myrow1=mysqli_query($db, "SELECT * FROM time_eat");
+		$rezult1=mysqli_fetch_array($myrow1);
+		do {
+				printf ("<option value=\"%s\">%s</option>", $rezult1['id'], $rezult1['title']);
+			}
+			while ($rezult1=mysqli_fetch_array($myrow1));
+			?>
+		</select>
 		<input type="submit" name="save_food" value="Сохранить">
 	</form>
 <?elseif ($_POST['id']):?>
@@ -39,11 +49,62 @@
 	?>
 <?else:?>
 	<? 
+	$myrow_price_meal=mysqli_query($db,"SELECT price FROM meal");
+	$rezult_price_meal=mysqli_fetch_array($myrow_price_meal);
+	?>
+	<h2>Общая стоимость блюд: 
+	<?
+//	$sum_price=0;
+//	$i=0;
+//		do {
+//			$sum_price=$sum_price+$rezult_price_meal[$i];
+//			$i++;
+//		}
+//	while ($i<=count($rezult_price_meal)) ;
+//	echo $sum_price;
+	echo array_sum($rezult_price_meal),' </h2>';
+	?>
+	<h2>Общее количество блюд: 
+	<?
+	echo count($rezult_price_meal),' </h2>';
+	?>
+	<? 
 	$myrow_all_meal=mysqli_query($db,"SELECT * FROM meal");
 	$rezult_all_meal=mysqli_fetch_array($myrow_all_meal);
+	?>
+	<h1>Завтрак</h1>
+	<?
 	do {
-		printf ("<p> <a href='all_meal.php?id=%s'>%s %s %s %s </a></p>", $rezult_all_meal['id'], $rezult_all_meal['food'], $rezult_all_meal['weight'], $rezult_all_meal['price'], $rezult_all_meal['time_id'] );
-	}
+		if ($rezult_all_meal['time_id']==1) {
+			printf ("<p> <a href='all_meal.php?id=%s'>%s %s %s %s </a></p>", $rezult_all_meal['id'], $rezult_all_meal['food'], $rezult_all_meal['weight'], $rezult_all_meal['price'], $rezult_all_meal['time_id'] );
+		}
+		}
+	while ($rezult_all_meal=mysqli_fetch_array($myrow_all_meal)) ;
+	?>
+	<? 
+	$myrow_all_meal=mysqli_query($db,"SELECT * FROM meal");
+	$rezult_all_meal=mysqli_fetch_array($myrow_all_meal);
+	?>
+	<h1>Обед</h1>
+	<?
+	do {
+		if ($rezult_all_meal['time_id']==2) {
+			printf ("<p> <a href='all_meal.php?id=%s'>%s %s %s %s </a></p>", $rezult_all_meal['id'], $rezult_all_meal['food'], $rezult_all_meal['weight'], $rezult_all_meal['price'], $rezult_all_meal['time_id'] );
+		}
+		}
+	while ($rezult_all_meal=mysqli_fetch_array($myrow_all_meal)) ;
+	?>
+	<? 
+	$myrow_all_meal=mysqli_query($db,"SELECT * FROM meal");
+	$rezult_all_meal=mysqli_fetch_array($myrow_all_meal);
+	?>
+	<h1>Ужин</h1>
+	<?
+	do {
+		if ($rezult_all_meal['time_id']==3) {
+			printf ("<p> <a href='all_meal.php?id=%s'>%s %s %s %s </a></p>", $rezult_all_meal['id'], $rezult_all_meal['food'], $rezult_all_meal['weight'], $rezult_all_meal['price'], $rezult_all_meal['time_id'] );
+		}
+		}
 	while ($rezult_all_meal=mysqli_fetch_array($myrow_all_meal)) ;
 	?>
 <?endif?>
--- index.php
+++ index.php
@@ -11,6 +11,20 @@
 <h2><a href="../admin/add_meal.php">Занести блюдо в меню</a></h2>
 <h2><a href="../admin/all_meal.php">Меню</a></h2>
 <h2><a href="../admin/del_meal.php">Удаление</a></h2>
+<? $i=1;
+$max_myrow=$myrow[1];
+do {
+	$myrow[$i]=random_int(1, 7);
+	echo '<p> ',$myrow[$i], ' ромашка, </p>';
+	if ($myrow[$i]>$max_myrow) {
+		$max_myrow=$myrow[$i];}
+	
+	$i=$i+1;
+}
+while ($i<=10);
+echo "Количество элементов: ",count($myrow), '<br>максимум: ', $max_myrow;
+?>
+
 </div>
 </body>
 </html>
